ce282adf95a0fa5f42421d3d25b19bddff609b3a
thangngoc849 <thangngoc849@gmail.com>
2025-04-25 09:36:06 +0700

    Draw mesh

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 const int SCREEN_WIDTH = 300;
 const int SCREEN_HEIGHT = 300;
+const int CELL_SIZE = 100;
 
 int main(int argc, char* argv[]) {
     SDL_Init(SDL_INIT_VIDEO);
@@ -11,12 +12,19 @@ int main(int argc, char* argv[]) {
 
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
 
-    SDL_Delay(2000); // Dừng 2 giây để xem cửa sổ
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    for (int i = 1; i < 3; ++i) {
+        SDL_RenderDrawLine(renderer, i * CELL_SIZE, 0, i * CELL_SIZE, SCREEN_HEIGHT); // Dọc
+        SDL_RenderDrawLine(renderer, 0, i * CELL_SIZE, SCREEN_WIDTH, i * CELL_SIZE);   // Ngang
+    }
+
+    SDL_RenderPresent(renderer);
+    SDL_Delay(2000);
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
 }
+
