002c4bf8a4eacefe6e090d0b04dc1902a6bfd138
thangngoc849 <thangngoc849@gmail.com>
2025-04-25 23:17:24 +0700

    Draw XO

--- main.cpp
+++ main.cpp
@@ -4,6 +4,26 @@ const int SCREEN_WIDTH = 300;
 const int SCREEN_HEIGHT = 300;
 const int CELL_SIZE = 100;
 
+void drawX(SDL_Renderer* renderer, int row, int col) {
+    int x = col * CELL_SIZE;
+    int y = row * CELL_SIZE;
+    SDL_SetRenderDrawColor(renderer, 200, 0, 0, 255);
+    SDL_RenderDrawLine(renderer, x + 10, y + 10, x + CELL_SIZE - 10, y + CELL_SIZE - 10);
+    SDL_RenderDrawLine(renderer, x + CELL_SIZE - 10, y + 10, x + 10, y + CELL_SIZE - 10);
+}
+
+void drawO(SDL_Renderer* renderer, int row, int col) {
+    int x = col * CELL_SIZE + CELL_SIZE / 2;
+    int y = row * CELL_SIZE + CELL_SIZE / 2;
+    int r = CELL_SIZE / 2 - 10;
+    SDL_SetRenderDrawColor(renderer, 0, 0, 200, 255);
+    for (int w = 0; w < r * 2; ++w)
+        for (int h = 0; h < r * 2; ++h)
+            if ((r - w) * (r - w) + (r - h) * (r - h) <= r * r &&
+                (r - w) * (r - w) + (r - h) * (r - h) >= (r - 2) * (r - 2))
+                SDL_RenderDrawPoint(renderer, x + w - r, y + h - r);
+}
+
 int main(int argc, char* argv[]) {
     SDL_Init(SDL_INIT_VIDEO);
     SDL_Window* window = SDL_CreateWindow("Tic Tac Toe", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
@@ -19,8 +39,11 @@ int main(int argc, char* argv[]) {
         SDL_RenderDrawLine(renderer, 0, i * CELL_SIZE, SCREEN_WIDTH, i * CELL_SIZE);   // Ngang
     }
 
+    drawX(renderer, 0, 0);
+    drawO(renderer, 1, 1);
+
     SDL_RenderPresent(renderer);
-    SDL_Delay(2000);
+    SDL_Delay(10000);
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
