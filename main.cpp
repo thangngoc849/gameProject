7f58ec281371be8d136959918f54970a62d8f43e
thangngoc849 <thangngoc849@gmail.com>
2025-04-26 22:35:48 +0700

    Click XO

--- main.cpp
+++ main.cpp
@@ -1,10 +1,31 @@
 ﻿#include <SDL.h>
+#include <iostream>
 
 const int SCREEN_WIDTH = 300;
 const int SCREEN_HEIGHT = 300;
 const int CELL_SIZE = 100;
 
-void drawX(SDL_Renderer* renderer, int row, int col) {
+SDL_Window* window = nullptr;
+SDL_Renderer* renderer = nullptr;
+char board[3][3];
+char current_marker = 'X';
+int current_player = 1;
+
+void resetBoard() {
+    for (int i = 0; i < 3; ++i)
+        for (int j = 0; j < 3; ++j)
+            board[i][j] = ' ';
+}
+
+void drawGrid() {
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    for (int i = 1; i < 3; ++i) {
+        SDL_RenderDrawLine(renderer, i * CELL_SIZE, 0, i * CELL_SIZE, SCREEN_HEIGHT);
+        SDL_RenderDrawLine(renderer, 0, i * CELL_SIZE, SCREEN_WIDTH, i * CELL_SIZE);
+    }
+}
+
+void drawX(int row, int col) {
     int x = col * CELL_SIZE;
     int y = row * CELL_SIZE;
     SDL_SetRenderDrawColor(renderer, 200, 0, 0, 255);
@@ -12,7 +33,7 @@ void drawX(SDL_Renderer* renderer, int row, int col) {
     SDL_RenderDrawLine(renderer, x + CELL_SIZE - 10, y + 10, x + 10, y + CELL_SIZE - 10);
 }
 
-void drawO(SDL_Renderer* renderer, int row, int col) {
+void drawO(int row, int col) {
     int x = col * CELL_SIZE + CELL_SIZE / 2;
     int y = row * CELL_SIZE + CELL_SIZE / 2;
     int r = CELL_SIZE / 2 - 10;
@@ -24,30 +45,61 @@ void drawO(SDL_Renderer* renderer, int row, int col) {
                 SDL_RenderDrawPoint(renderer, x + w - r, y + h - r);
 }
 
-int main(int argc, char* argv[]) {
-    SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window* window = SDL_CreateWindow("Tic Tac Toe", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                                          SCREEN_WIDTH, SCREEN_HEIGHT, 0);
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
+void drawBoard() {
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderClear(renderer);
 
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    for (int i = 1; i < 3; ++i) {
-        SDL_RenderDrawLine(renderer, i * CELL_SIZE, 0, i * CELL_SIZE, SCREEN_HEIGHT); // Dọc
-        SDL_RenderDrawLine(renderer, 0, i * CELL_SIZE, SCREEN_WIDTH, i * CELL_SIZE);   // Ngang
-    }
+    drawGrid();
 
-    drawX(renderer, 0, 0);
-    drawO(renderer, 1, 1);
+    for (int i = 0; i < 3; ++i)
+        for (int j = 0; j < 3; ++j) {
+            if (board[i][j] == 'X') drawX(i, j);
+            else if (board[i][j] == 'O') drawO(i, j);
+        }
 
     SDL_RenderPresent(renderer);
-    SDL_Delay(10000);
+}
+
+bool placeMarker(int row, int col) {
+    if (board[row][col] == ' ') {
+        board[row][col] = current_marker;
+        return true;
+    }
+    return false;
+}
+
+void handleMouseClick(int x, int y) {
+    int row = y / CELL_SIZE;
+    int col = x / CELL_SIZE;
+
+    if (row >= 0 && row < 3 && col >= 0 && col < 3 && placeMarker(row, col)) {
+        current_marker = (current_marker == 'X') ? 'O' : 'X';
+        current_player = (current_player == 1) ? 2 : 1;
+    }
+}
+
+int main(int argc, char* argv[]) {
+    SDL_Init(SDL_INIT_VIDEO);
+    window = SDL_CreateWindow("Tic Tac Toe", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                              SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    resetBoard();
+
+    bool running = true;
+    SDL_Event event;
+    while (running) {
+        drawBoard();
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT)
+                running = false;
+            else if (event.type == SDL_MOUSEBUTTONDOWN && event.button.button == SDL_BUTTON_LEFT)
+                handleMouseClick(event.button.x, event.button.y);
+        }
+    }
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
 }
-
